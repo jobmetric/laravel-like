3143ffcc81f0f96fd68c6346b25ef248e07b4deb
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-13 17:29:08 +0330

    add like service provider

---
+++ src/LikeServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace JobMetric\Like;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class LikeServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-like')
+            ->hasConfig()
+            ->hasTranslation()
+            ->hasMigration();
+    }
+}
